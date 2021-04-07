18692c3ba3e587d2c2c1baf72ed010234a92c413
Victoria A <victoria.arriola@northwestern.edu>
2021-04-06 23:59:49 -0500

    list of candidates

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -2,26 +2,20 @@ import os
 import csv
 
 
-
+# path to csv file
 input_file = os.path.join("Resources", "Homework_03-Python_Instructions_PyPoll_Resources_election_data.csv")
-# print(input_file)
-output_file = os.path.join("analysis", "election_analysis.txt")
+
 
 # create global variables
 vote_total = 0
-percent_votes = 0
-winner_won = 0
-name = 0
-vote = 0
-# dict to store candidates name
-candidates = {}
-candidates_votes = {}
+candidates_list = []
+candidates_votes = []
 
 
 
 
 
-# outter loop
+# read file
 with open(input_file) as election_data:
     reader = csv.reader(election_data, delimiter = ",")
     header = next(reader)
@@ -30,11 +24,20 @@ with open(input_file) as election_data:
 # read in total votes
         vote_total = vote_total +1 
 # read in candidate name 
-        candidate_name = (row[2])
-# adding in vote count per candidate?
-        if candidate_name in candidates :
-            candidate_votes = candidate_votes + 1
-        print(f"Candidate votes: {candidate_votes}")
+        candidates_name = (row[2])
+# list of candidates
+        if candidates_name in candidates_list :
+            candidate_index = candidates_list.index(candidates_name)
+            candidates_votes[candidate_index] = candidates_votes[candidate_index] +1
+        else:
+            candidates_list.append(candidates_name)
+            candidates_votes.append(1)
+
+            
+print(f'{vote_total}')
+print(f'{candidates_list}')
+
+
 
 
         # remaing loop
@@ -44,20 +47,22 @@ with open(input_file) as election_data:
 
 # i want to get the names of every candidate
     
-def candidates_votes(candidate_data):
-    name = str(candidate_data[2])
-    votes = int(candidate_data)
+# def candidates_votes(candidate_data):
+#     name = str(candidate_data[2])
+#     votes = int(candidate_data)
     
-#     if name == name :
-#         votes = vote + 1
-#     print(f"candidate names:{votes}")
-#  percent of votes   
-percent_votes_candidate = (won_vote/ vote_total)
+# #     if name == name :
+# #         votes = vote + 1
+# #     print(f"candidate names:{votes}")
+# #  percent of votes   
+# percent_votes_candidate = (won_vote/ vote_total)
 
 # total votes each candidate won 
-candidate_votes = 
+# candidate_votes = 
+# # print(input_file)
+# output_file = os.path.join("analysis", "election_analysis.txt")
 
 # print("Election Results")
 # print("------------------------------------------")
- # print(f"Total Votes:{vote_total}")
+#  print(f"Total Votes:{vote_total}")
 #  print("------------------------------------------")
