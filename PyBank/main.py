a1a618cda550967c388ea0e68dedba6204fb2023
Victoria A <victoria.arriola@northwestern.edu>
2021-04-08 20:00:17 -0500

    starting

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,10 +2,13 @@ import os
 
 import csv
 
-csvpath = os.path.join("..",'Resources', 'Homework_03-Python_Instructions_PyBank_Resources_budget_data.csv')
+# path to csv file
+csvpath = os.path.join("Resources", "Homework_03-Python_Instructions_PyBank_Resources_budget_data")
 
 print(csvpath)
 
+
+# read file
 with open(csvpath,'r') as file_handler:
     lines = file_handler.read()
     print(lines)
