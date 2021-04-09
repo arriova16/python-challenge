497479cbe7fb5a10b52bd6e1639f16b6c7bd382e
Victoria A <victoria.arriola@northwestern.edu>
2021-04-08 21:24:43 -0500

    total amount

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,4 +1,4 @@
-# The total number of months included in the dataset
+# The total number of months included in the dataset-
 # The net total amount of "Profit/Losses" over the entire period
 # The average of the changes in "Profit/Losses" over the entire period
 # The greatest increase in profits (date and amount) over the entire period
@@ -30,17 +30,20 @@ with open(csvpath) as budget_data:
 
 
     for row in reader:
+    #    total months 
         total_months = total_months + 1
-print(total_months)
-
+# total of profits and losses
+        total_amount = total_amount + float(row[1])
+# print(total_months)
+# print(f'{total_amount}')
 
 # output_file = os.path.join("budget_data.txt")
 # with open(output_file, "w", newline = '') as datafile:
 
-# print("Financial Analysis")
-# print("----------------------")
-# print("Total Months :" + total_months)
-# print("Total: " + total_amount)
+print("Financial Analysis")
+print("----------------------")
+print(f'Total Months : {total_months}')
+print(f'Total: ${total_amount}')
 # print("Average Change:" + changes)
 # print("Greatest Increase in Profits:"_____ )
 # print("Greatest Decrease in Profits:"_____ )
