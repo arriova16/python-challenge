58974f8c1312e19f82f472f6e027bf68eb80062f
Victoria A <victoria.arriola@northwestern.edu>
2021-04-09 12:48:30 -0500

    text file

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,5 +1,5 @@
 # The total number of months included in the dataset-
-# The net total amount of "Profit/Losses" over the entire period
+# The net total amount of "Profit/Losses" over the entire period-
 # The average of the changes in "Profit/Losses" over the entire period
 # The greatest increase in profits (date and amount) over the entire period
 # The greatest decrease in losses (date and amount) over the entire period
@@ -20,9 +20,10 @@ changes = 0
 lowest_month = 0
 highest_month = 0
 
-# any list or dict 
-
-
+# any list 
+profit_change = []
+total_months_profits = []
+total_profit = []
 # read file
 with open(csvpath) as budget_data:
     reader = csv.reader(budget_data)
@@ -30,20 +31,45 @@ with open(csvpath) as budget_data:
 
 
     for row in reader:
+        profit_losses = float(row[1])
     #    total months 
         total_months = total_months + 1
 # total of profits and losses
-        total_amount = total_amount + float(row[1])
-# print(total_months)
-# print(f'{total_amount}')
+        total_amount = round(total_amount + profit_losses)
+ 
+        
+#  print(total_months)
+# print(f'${total_amount}')
+   
+        
 
-# output_file = os.path.join("budget_data.txt")
-# with open(output_file, "w", newline = '') as datafile:
+# i want the average change
+
+
+# max and min function 
+# greatest increase
+# i need a month and an amount
+# highest_month = max(profit_change)
+
+# greatest decrease
+# lowest_month = min(profit_change)
+
 
 print("Financial Analysis")
 print("----------------------")
 print(f'Total Months : {total_months}')
 print(f'Total: ${total_amount}')
-# print("Average Change:" + changes)
-# print("Greatest Increase in Profits:"_____ )
-# print("Greatest Decrease in Profits:"_____ )
+print("Average Change: $")
+print("Greatest Increase in Profits:")
+print("Greatest Decrease in Profits:")
+
+output_file = os.path.join("budget_data.txt")
+with open(output_file, "w", newline = '') as datafile:
+
+    datafile.write("Financial Analysis\n")
+    datafile.write("----------------------\n")
+    datafile.write(f'Total Months : {total_months}\n')
+    datafile.write(f'Total: ${total_amount}\n')
+    datafile.write('Average Change: $\n')
+    datafile.write('Greatest Increase in Profits:\n')
+    datafile.write('Greatest Decrease in Profits: \n')
